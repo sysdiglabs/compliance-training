6977dff574b5bf6acfece2a12283adb5be8ad653
Michael Scholl <michael.scholl@sysdig.com>
2023-01-26 09:38:45 -0800

    Fixing deployment and service.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,9 @@
-
+resource "random_id" "sko_bucket" {
+  byte_length = 8
+}
 
 resource "aws_s3_bucket" "sko_bucket" {
-  bucket_prefix = "sysdig-sko-bucket"
+  bucket = "sysdig-sko-bucket-${random_id.sko_bucket.hex}"
 }
 
 resource "aws_s3_bucket_public_access_block" "sko_bucket" {
@@ -13,6 +15,8 @@ resource "aws_s3_bucket_public_access_block" "sko_bucket" {
   restrict_public_buckets = false
 }
 
+/*
+ToDo: Team needs to make changes to the check before implementing this violation.
 resource "aws_security_group" "sko_compliance_violation" {
   vpc_id      = module.vpc.vpc_id
   name = "sko_compliance_violation"
@@ -33,3 +37,4 @@ resource "aws_security_group" "sko_compliance_violation" {
     Name = "sko_compliance_violation"
   }
 }
+*/
