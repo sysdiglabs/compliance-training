0f1aec8c1950520e51fa51822b5ce5a60dd026d3
Gary Hutchins <gary.hutchins@sysdig.com>
2023-01-27 13:00:32 -0500

    Added instructions for terraform

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -7,7 +7,18 @@ terraform {
 }
 
 provider "sysdig" {
-  sysdig_secure_url        = "<SYSDIG_URL>"
+  # Comment out the correct Secure URL for your region
+  #
+  #sysdig_secure_url        = "https://secure.sysdig.com" #US East
+  #sysdig_secure_url        = "https://us2.app.sysdig.com" #US West (US2)
+  #sysdig_secure_url        = "https://us4.app.sysdig.com" #US West (US4-GCP)
+  #sysdig_secure_url        = "https://eu1.app.sysdig.com" #EU 1 
+  #sysdig_secure_url        = "https://app.au1.sysdig.com" #AP 1 
+  #
+  #If your region is not listed, comment this line out and enter your SaaS secure URL
+  #sysdig_secure_url        = "<SYSDIG_URL>"
+
+  #Enter your secure API Token here. Can be found in you SaaS account under settings/user profile
   sysdig_secure_api_token  = "<SYSDIG_API_TOKEN>"
 }
 
