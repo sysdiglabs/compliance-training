587f4fac57fc4d17197fbff1ca8daf4d25ea5a9d
Gary Hutchins <gary.hutchins@sysdig.com>
2023-06-12 15:56:30 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,7 +10,7 @@ resource "aws_s3_bucket_public_access_block" "sko_bucket" {
   bucket = aws_s3_bucket.sko_bucket.id
 
   block_public_acls       = false
-  block_public_policy     = false
+  block_public_policy     = true
   ignore_public_acls      = false
   restrict_public_buckets = false
 }
